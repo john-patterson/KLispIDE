46b6b70aa16f988e4328db55eb876ff39190445a
John Patterson <john@johnppatterson.com>
2020-10-19 23:43:12 -0700

    almost working versinon of editor highlighting by token

--- editor/src/main/kotlin/EditorMain.kt
+++ editor/src/main/kotlin/EditorMain.kt
@@ -1,6 +1,7 @@
 package com.statlesscoder.klisp.editor
 import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
+import com.google.gson.Gson
 import com.statelesscoder.klisp.compiler.Token
 import javafx.concurrent.Task
 import javafx.scene.paint.Color
@@ -27,6 +28,14 @@ class EditorApp : App(EditorView::class, Styles::class)
 class Styles : Stylesheet() {
     companion object {
         val command by cssclass()
+        val class_right_parens by cssclass()
+        val class_left_parens by cssclass()
+        val class_numeric by cssclass()
+        val class_boolean by cssclass()
+        val class_identifier by cssclass()
+        val class_let by cssclass()
+        val class_if by cssclass()
+        val class_fun by cssclass()
     }
 
     init {
@@ -34,6 +43,30 @@ class Styles : Stylesheet() {
             fill = Color.RED
             fontSize = 40.px
             fontWeight = FontWeight.BOLD
+        }
+        class_right_parens {
+            fill = Color.GREEN
+        }
+        class_left_parens {
+            fill = Color.GREEN
+        }
+        class_numeric {
+            fill = Color.ORANGE
+        }
+        class_boolean {
+            fill = Color.ORANGE
+        }
+        class_identifier {
+            fill = Color.BLUE
+        }
+        class_let {
+            fill = Color.PURPLE
+        }
+        class_if {
+            fill = Color.PURPLE
+        }
+        class_fun {
+            fill = Color.PURPLE
         }
         root {
             prefHeight = 600.px
@@ -63,50 +96,15 @@ class EditorView: View() {
                     }
                 }
                 .subscribe {
-                    label.text = it
-                    setStyleClass(0, length, "command")
+                    setStyleSpans(0, it)
                 }
         }
     }
 
-//    fun computeHighlightingAsync(): Task<StyleSpans<Collection<String>>> {
-//        val task = object : Task<StyleSpans<Collection<String>>>() {
-//            override fun call(): StyleSpans<Collection<String>> {
-//                return computeHighlighting(codeArea.text)
-//            }
-//        }
-//
-//        executor.execute(task)
-//        return task
-//    }
-//
-//    fun computeHighlighting(text: String): StyleSpans<Collection<String>> {
-//        var builder = StyleSpansBuilder<Collection<String>>()
-//        val httpAsync = "http://localhost:7340/tokenize"
-//            .httpPost()
-//            .body(text)
-//            .responseString { request, response, result ->
-//                when (result) {
-//                    is Result.Failure -> {
-//                        val ex = result.getException()
-//                        println(ex)
-//                    }
-//                    is Result.Success -> {
-//                        val data = result.get()
-//                        println(data)
-//                        builder.add(Collections.singleton("command"), data.length)
-//                    }
-//                }
-//            }
-//
-//        httpAsync.join()
-//        return builder.create()
-//    }
-
-    fun computeHighlightingAsync(): Task<String> {
-        val task = object : Task<String>() {
-            override fun call(): String {
-                return tokenize(codeArea.text)
+    fun computeHighlightingAsync(): Task<StyleSpans<Collection<String>>> {
+        val task = object : Task<StyleSpans<Collection<String>>>() {
+            override fun call(): StyleSpans<Collection<String>> {
+                return computeHighlighting(codeArea.text)
             }
         }
 
@@ -114,23 +112,63 @@ class EditorView: View() {
         return task
     }
 
-    fun tokenize(text: String): String {
-        var resultingTokens: String = ""
+    fun computeHighlighting(text: String): StyleSpans<Collection<String>> {
+        var builder = StyleSpansBuilder<Collection<String>>()
         val httpAsync = "http://localhost:7340/tokenize"
             .httpPost()
             .body(text)
             .responseString { request, response, result ->
                 when (result) {
                     is Result.Failure -> {
-                        resultingTokens = result.getException().toString()
+                        val ex = result.getException()
+                        println(ex)
                     }
                     is Result.Success -> {
-                        resultingTokens = result.get()
+                        val data = result.get()
+                        val tokens = Gson().fromJson(data, Array<Token>::class.java)
+                        var lastIdx = 0
+                        for (token in tokens) {
+                            val className = "class_${token.type.toString().toLowerCase()}"
+                            builder.add(Collections.emptyList(), token.pos - lastIdx)
+                            builder.add(Collections.singleton(className), token.text.length)
+                            lastIdx = token.pos
+                        }
                     }
                 }
             }
 
         httpAsync.join()
-        return resultingTokens
+        return builder.create()
     }
+
+//    fun computeHighlightingAsync(): Task<String> {
+//        val task = object : Task<String>() {
+//            override fun call(): String {
+//                return tokenize(codeArea.text)
+//            }
+//        }
+//
+//        executor.execute(task)
+//        return task
+//    }
+//
+//    fun tokenize(text: String): String {
+//        var resultingTokens: String = ""
+//        val httpAsync = "http://localhost:7340/tokenize"
+//            .httpPost()
+//            .body(text)
+//            .responseString { request, response, result ->
+//                when (result) {
+//                    is Result.Failure -> {
+//                        resultingTokens = result.getException().toString()
+//                    }
+//                    is Result.Success -> {
+//                        resultingTokens = result.get()
+//                    }
+//                }
+//            }
+//
+//        httpAsync.join()
+//        return resultingTokens
+//    }
 }
