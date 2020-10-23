985e0da3e4545dbc3bc3b5f5633c9872fcd76fd4
John Patterson <john@johnppatterson.com>
2020-10-22 19:51:19 -0700

    extract Function

--- compiler/src/main/kotlin/Data.kt
+++ compiler/src/main/kotlin/Data.kt
@@ -2,29 +2,6 @@ package com.statelesscoder.klisp.compiler
 
 class RuntimeException(message: String): Exception(message)
 
-class Function(private val executor: Executor,
-               private val name: String,
-               private val params: List<ExpressionPart>,
-               private val body: ExpressionPart) {
-    fun run(args: List<Data>, scope: Scope = Scope()): Data {
-        if (args.size != params.size) {
-            throw RuntimeException("Function '$name' expects '${params.size}' arguments, but got '${args.size}'.")
-        }
-
-        val boundScope = Scope(scope)
-        for (i in args.indices) {
-            boundScope.add(params[i].name!!, args[i])
-        }
-
-        return when (body.type) {
-            ExpressionPartType.BOOLEAN, ExpressionPartType.NUMBER, ExpressionPartType.SYMBOL, ExpressionPartType.STRING ->
-                executor.realizePart(body, boundScope).data
-            ExpressionPartType.EXPRESSION, ExpressionPartType.KEYWORD ->
-                executor.execute(body.expression!!, boundScope).data
-        }
-    }
-}
-
 
 enum class DataType {
     STRING,
