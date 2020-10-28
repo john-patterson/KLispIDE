00c83513657de2c45720e9a31f5ba80c96ec73db
John Patterson <john@JohnPPatterson.com>
2020-10-27 18:25:11 -0700

    create constructors for ExpressionPart to replace helper functions

--- compiler/src/main/kotlin/types/ExpressionPart.kt
+++ compiler/src/main/kotlin/types/ExpressionPart.kt
@@ -1,6 +1,9 @@
 package com.statelesscoder.klisp.compiler.types
 
-class ExpressionPart(val type: ExpressionPartType) {
+import com.statelesscoder.klisp.compiler.Symbol
+
+open class ExpressionPart {
+    val type: ExpressionPartType;
     var value: Float? = null
     var truth: Boolean? = null
     var name: String? = null
@@ -8,6 +11,46 @@ class ExpressionPart(val type: ExpressionPartType) {
     var keywordType: KeywordType? = null
     var expression: Expression? = null
     var list: KList? = null
+
+    constructor(value: Float)
+        : this(ExpressionPartType.NUMBER) {
+            this.value = value
+    }
+
+    constructor(value: Boolean)
+            : this(ExpressionPartType.BOOLEAN) {
+        this.truth = value
+    }
+
+    constructor(value: String)
+        : this(ExpressionPartType.STRING) {
+            this.innerText = value
+    }
+
+    constructor(value: Expression)
+            : this(ExpressionPartType.EXPRESSION) {
+        this.expression = value
+    }
+
+    constructor(value: KList)
+            : this(ExpressionPartType.LIST) {
+        this.list = value
+    }
+
+    constructor(value: KeywordType)
+            : this(ExpressionPartType.KEYWORD) {
+        this.keywordType = value
+    }
+
+    constructor(value: Symbol)
+            : this(ExpressionPartType.SYMBOL) {
+        this.name = value.symbolName
+    }
+
+    constructor(type: ExpressionPartType)
+    {
+        this.type = type
+    }
 
     override fun toString(): String = when(type) {
         ExpressionPartType.NUMBER -> value.toString()
