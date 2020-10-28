bf06ed44b8eba5bcd254ee89ccc8a62874a7a11e
John Patterson <john@JohnPPatterson.com>
2020-10-27 17:58:29 -0700

    creates basic version of IfExpression and LetBinding classes
    This will allow a more rich form of understanding these types of expressions once they are parsed. The form of an IfExpression or LetBinding doesn't need to be duplicated in the Parser and in the Executor. Once the parser knows something is a let binding or an if expression, it should encode that knowledge into the type system itself.

--- compiler/src/main/kotlin/types/Expression.kt
+++ compiler/src/main/kotlin/types/Expression.kt
@@ -1,6 +1,13 @@
 package com.statelesscoder.klisp.compiler.types
 
-data class Expression(val head: ExpressionPart, val tail: List<ExpressionPart>) {
+import com.statelesscoder.klisp.compiler.Executor
+import com.statelesscoder.klisp.compiler.Scope
+
+open class Expression(val head: ExpressionPart, val tail: List<ExpressionPart>) {
+    open fun execute(executor: Executor, scope: Scope): Data {
+        return executor.execute(this, scope)
+    }
+
     override fun toString(): String {
         val headString = head.toString()
         val tailString = tail.joinToString(separator = " ") { it.toString() }
